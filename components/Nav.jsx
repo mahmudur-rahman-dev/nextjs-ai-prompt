9b970fced2f05ba8ef6d0a239d9791665c30869f
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2024-08-06 23:37:29 +0600

    refactor the nav component add sign out button and sign in option as well add menu links

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,20 +2,19 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-// import { signIn, signOut, useSession, getProviders } from "next/auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
-    };
-
-    setProviders();
-  }, []);
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  });
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -58,40 +57,69 @@ const Nav = () => {
           </div>
         ) : (
           <>
-            {/* {providers && 
+            {providers && 
             Object.values(providers).map((provider) => (
               <button
               type="button"
-              key={provider.id}
+              key={provider.name}
               onClick={() => signIn(provider.id)}
               className="black_btn">
               Sign In
               </button>
-            ))} */}
-            
-            <button
-            className="black_btn">
-            Sign In
-            </button>
+            ))}
+
+            <button className="black_btn">Sign In</button>
           </>
         )}
       </div>
-      
+
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-          {isUserLoggedIn ?
-            <div className="flex">
+        {isUserLoggedIn ? (
+          <div className="flex">
             <Image
-                src="/assets/images/logo.svg"
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-                onClick={() => {}}
-              />
-            </div>
-            : (<>
-            {/* {providers && 
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => {
+                setToggleDropdown((prev) => !prev);
+              }}
+            />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                type="button"
+                onClick={() => {
+                  setToggleDropdown(false);
+                  // signOut();
+                }}
+                className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers && 
             Object.values(providers).map((provider) => (
               <button
               type="button"
@@ -100,17 +128,12 @@ const Nav = () => {
               className="black_btn">
               Sign In
               </button>
-            ))} */}
-            
-            <button
-            className="black_btn">
-            Sign In
-            </button>
+            ))}
 
-            </>)
-          }
+            <button className="black_btn">Sign In</button>
+          </>
+        )}
       </div>
-
     </nav>
   );
 };
